7cf298abe67e664651a1122f70dac638187cfed1
cycjimmy <cycjimmy@gmail.com>
2022-03-26 21:52:59 +0800

    fix: fix root version after releasing

--- release.config.cjs
+++ release.config.cjs
@@ -1,8 +1,8 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-const makeCommon = require('@cycjimmy/config-lib/cjs/semanticRelease/19.x/makeConfigWithPgkRootForLibrary.cjs').default;
+const makeConfig = require('@cycjimmy/config-lib/cjs/semanticRelease/19.x/makeConfigWithPgkRootForLibrary.cjs').default;
 const pkg = require('./package.json');
 
-module.exports = makeCommon({
+module.exports = makeConfig({
   githubOptions: {
     assets: [pkg.browser],
   },
